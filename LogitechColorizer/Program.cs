c9a28f34f5af0c0085612816b36d3159d5b4f806
Niklas Dahlman <nd@sql8r.net>
2015-12-31 16:59:04 +0100

    more testing

--- LogitechColorizer/Program.cs
+++ LogitechColorizer/Program.cs
@@ -13,36 +13,35 @@ namespace LogitechColorizer
         {
             if (LogitechGSDK.LogiLedInit())
             {
-                var m_targetDevice = 0;
-                //m_targetDevice |= LogitechGSDK.LOGI_DEVICETYPE_RGB;
-                //m_targetDevice |= LogitechGSDK.LOGI_DEVICETYPE_MONOCHROME;
-                m_targetDevice |= LogitechGSDK.LOGI_DEVICETYPE_PERKEY_RGB;
+                var m_targetDevice = LogitechGSDK.LOGI_DEVICETYPE_ALL;
 
-                LogitechGSDK.LogiLedSetTargetDevice(m_targetDevice);
-
-
-                //LogitechGSDK.LogiLedSaveCurrentLighting();
-                //LogitechGSDK.LogiLedSetLighting(10, 10, 100);
-                //LogitechGSDK.LogiLedRestoreLighting();
-                LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(KeyboardNames.NUM_ZERO, 10, 100, 10);
-
-                LogitechGSDK.LogiLedSaveLightingForKey(KeyboardNames.NUM_ZERO);
-
-                LogitechGSDK.LogiLedFlashSingleKey(KeyboardNames.NUM_ZERO, 0, 100, 0, 5000, 500);
-                LogitechGSDK.LogiLedPulseSingleKey(KeyboardNames.NUM_ZERO, 0, 10, 0, 0, 100, 0, 2000, false);
-
-
-              //  LogitechGSDK.LogiLedSetTargetDevice(0);
-              //  LogitechGSDK.LogiLedSetLighting(10, 10, 100);
-
-                int maj = 0, min = 0, build = 0;
-                if (LogitechGSDK.LogiLedGetSdkVersion(ref maj, ref min, ref build))
+                if (LogitechGSDK.LogiLedSetTargetDevice(m_targetDevice))
                 {
-                    Console.WriteLine("GSDK Version v{0}.{1}.{2}", maj, min, build);
-                    Console.ReadKey();
+
+                    LogitechGSDK.LogiLedSetLighting(0, 100, 0);
+
+                    //LogitechGSDK.LogiLedSaveCurrentLighting();
+                    //LogitechGSDK.LogiLedSetLighting(10, 10, 100);
+                    //LogitechGSDK.LogiLedRestoreLighting();
+            //        LogitechGSDK.LogiLedSetLightingForKeyWithKeyName(KeyboardNames.NUM_ZERO, 10, 100, 10);
+
+            //        LogitechGSDK.LogiLedSaveLightingForKey(KeyboardNames.NUM_ZERO);
+
+//                LogitechGSDK.LogiLedFlashSingleKey(KeyboardNames.NUM_ZERO, 0, 100, 0, 5000, 500);
+            //        LogitechGSDK.LogiLedPulseSingleKey(KeyboardNames.NUM_ZERO, 0, 10, 0, 0, 100, 0, 2000, false);
+
+
+
+                    int maj = 0, min = 0, build = 0;
+                    if (LogitechGSDK.LogiLedGetSdkVersion(ref maj, ref min, ref build))
+                    {
+                        Console.WriteLine("GSDK Version v{0}.{1}.{2}", maj, min, build);
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                    }
                 }
 
-                // LogitechGSDK.LogiLedSaveCurrentLighting();
+
                 LogitechGSDK.LogiLedShutdown();
             }
         }
