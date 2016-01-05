759524d8a9d0f1790287369a5df54d3b46028ff2
Niklas Dahlman <nd@sql8r.net>
2016-01-05 20:36:39 +0100

    envDTE ref

---
+++ GSeriesColorizerReSharperPlugin/DevEnv.cs
@@ -0,0 +1,49 @@
+﻿using GSeriesColorizer.Lib;
+using EnvDTE;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Resources.Shell;
+
+namespace GSeriesColorizerReSharperPlugin
+{
+    [SolutionComponent]
+    public class DevEnv
+    {
+        public DevEnv()
+        {
+            _dte = Shell.Instance.GetComponent<DTE>();
+        }
+
+        private readonly DTE _dte;
+
+        private void SubscribeEvents()
+        {
+            var events = _dte.Events;
+            if (events == null)
+                return;
+
+            var buildEvents = events.BuildEvents;
+            if (buildEvents == null)
+                return;
+
+           
+                buildEvents.OnBuildBegin += OnBuildBegin;
+           
+               // buildEvents.OnBuildBegin -= OnBuildBegin;
+           
+
+           
+                buildEvents.OnBuildDone += OnBuildDone;
+           
+              //  buildEvents.OnBuildDone -= OnBuildDone;
+           
+        }
+
+        private void OnBuildBegin(vsBuildScope scope, vsBuildAction action)
+        {
+        }
+
+        private void OnBuildDone(vsBuildScope scope, vsBuildAction action)
+        {
+        }
+    }
+}
